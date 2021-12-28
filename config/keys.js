2d80b6a8cf480df6514e1b91a2d53ba9c0065e38
meanagha <iamanaghapatil@gmail.com>
2021-12-28 14:27:13 +0530

    db url changed

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,4 @@
 module.exports = {
-  mongoURI: "mongodb://localhost:27017/chat-app",
+  mongoURI: "mongodb+srv://anagha:ganapati123@cluster0.b2ybc.mongodb.net/chat-app?retryWrites=true&w=majority",
   secretOrKey: "secret",
 };
