6bace5ba9c75b1fe22b417cc54b59837433f5887
meanagha <iamanaghapatil@gmail.com>
2021-12-28 15:27:51 +0530

    mongodb url

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,4 @@
 module.exports = {
-  mongoURI: "mongodb+srv://anagha:ganapati123@cluster0.b2ybc.mongodb.net/chat-app?retryWrites=true&w=majority",
+  mongoURI: "mongodb://localhost:27017/chat-app",
   secretOrKey: "secret",
 };
